bfafac9b190c36c9d7c3edc1ddf38dde3005361b
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2022-02-05 16:13:55 +0530

    Update: create Assessments Types

---
+++ src/pages/assessments/types/OTAssessment.type.ts
@@ -0,0 +1,20 @@
+interface IOTAssessment {
+    assessmentType: "OT",
+    client_id: string,
+    therapist: string,
+    assessment_date: string,
+    presenting_complaints: string,
+    milestone_development: string,
+    behavior_cognition: string,
+    cognitive_skills: string,
+    kinaesthesia: string,
+    created_on: string,
+    created_by: string,
+    modified_on: string,
+    modified_by: string,
+    email_sent: boolean,
+    version: number,
+    draft: boolean,
+}
+
+export default IOTAssessment;
---
+++ src/pages/assessments/types/index.ts
@@ -0,0 +1,3 @@
+export type { default as IBTAssessment } from "./BTAssessment.type";
+export type { default as ISTAssessment } from "./STAssessment.type";
+export type { default as IOTAssessment } from "./OTAssessment.type";
