bb8195ead901d898656b85b5901c07ab0b398ff7
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2022-02-05 11:27:37 +0530

    Update: add edit and create assessment buttons

--- src/pages/clients/components/ClientItem.view.tsx
+++ src/pages/clients/components/ClientItem.view.tsx
@@ -22,20 +22,11 @@ function ClientItem(props: IProps) {
       </div>
       <div className="flex justify-center lg:justify-end items-center content-center space-x-2">
         <Link
-          to={`/dashboard/assessment/${props.client_id}`}
-          className="flex items-center py-4 col-span-1"
-        >
-          <Button secondary onClick={() => {}}>
-            <Label title="Edit Assessment" className="text-white" />
-          </Button>
-        </Link>
-
-        <Link
           to={`/dashboard/clients/edit/${props.client_id}`}
           className="flex items-center py-4 col-span-1"
         >
           <Button secondary onClick={() => {}}>
-            <Label title="Edit" className="text-white" />
+            <Label title="Edit Client" className="text-white" />
           </Button>
         </Link>
         <Button
@@ -44,11 +35,77 @@ function ClientItem(props: IProps) {
             props.onDeleteClick({ email: props.email });
           }}
         >
-          <Label title="Delete" className="text-white" />
+          <Label title="Delete Client" className="text-white" />
         </Button>
+      </div>
+
+      <div className="flex flex-row justify-center lg:justify-end items-center content-center space-x-2">
+        <CreateEditAssessmentsButtons
+          client_id={props.client_id}
+          assessment={props.assessment}
+          assessmentType="BT"
+          currentVersion={Number(props.bt)}
+        />
+
+        <CreateEditAssessmentsButtons
+          client_id={props.client_id}
+          assessment={props.assessment}
+          assessmentType="ST"
+          currentVersion={Number(props.st)}
+        />
+
+        <CreateEditAssessmentsButtons
+          client_id={props.client_id}
+          assessment={props.assessment}
+          assessmentType="OT"
+          currentVersion={Number(props.ot)}
+        />
       </div>
     </div>
   );
 }
 
+function CreateEditAssessmentsButtons({
+  client_id,
+  assessment,
+  assessmentType,
+  currentVersion,
+}: any) {
+  assessmentType = assessmentType.toUpperCase();
+  return (
+    <>
+      {assessment.includes(assessmentType) && (
+        <div className="flex flex-col w-full justify-center items-center">
+          <Link
+            to={`/dashboard/assessment/${client_id}/${assessmentType}/${
+              currentVersion + 1
+            }`}
+            className="flex items-center py-4 col-span-1"
+          >
+            <Button primary onClick={() => {}}>
+              <Label
+                title={`Create ${assessmentType} Assessment`}
+                className="text-white"
+              />
+            </Button>
+          </Link>
+
+          {currentVersion != -1 && (
+            <Link
+              to={`/dashboard/assessment/${client_id}/${assessmentType}/${currentVersion}`}
+              className="flex items-center py-4 col-span-1"
+            >
+              <Button secondary onClick={() => {}}>
+                <Label
+                  title={`Edit ${assessmentType} Assessment`}
+                  className="text-white"
+                />
+              </Button>
+            </Link>
+          )}
+        </div>
+      )}
+    </>
+  );
+}
 export default ClientItem;
