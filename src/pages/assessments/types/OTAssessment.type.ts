91c19f57a1e081b77302c34d9351ead94a2495aa
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2022-02-05 17:25:40 +0530

    Update: validation and fields OTForm

--- src/pages/assessments/types/OTAssessment.type.ts
+++ src/pages/assessments/types/OTAssessment.type.ts
@@ -1,6 +1,6 @@
 interface IOTAssessment {
-    assessmentType: "OT",
-    client_id: string,
+    assessmentType?: "OT",
+    client_id?: string,
     therapist: string,
     assessment_date: string,
     presenting_complaints: string,
@@ -8,13 +8,13 @@ interface IOTAssessment {
     behavior_cognition: string,
     cognitive_skills: string,
     kinaesthesia: string,
-    created_on: string,
-    created_by: string,
-    modified_on: string,
-    modified_by: string,
-    email_sent: boolean,
-    version: number,
-    draft: boolean,
+    created_on?: string,
+    created_by?: string,
+    modified_on?: string,
+    modified_by?: string,
+    email_sent?: boolean,
+    version?: number,
+    draft?: boolean,
 }
 
 export default IOTAssessment;
