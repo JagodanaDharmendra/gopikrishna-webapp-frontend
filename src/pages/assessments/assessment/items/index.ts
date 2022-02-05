f1c350fb62786ad08e0aa54ed8b24c1aad32c17b
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2022-02-05 16:12:57 +0530

    Update: remove unwanted files

--- src/pages/assessments/assessment/items/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ItemCompletedAssessment } from "./ItemCompletedAssessment";
-export { default as ItemDraftAssessment } from "./ItemDraftAssessment";
-export { default as ItemPendingAssessment } from "./ItemPendingAssessment";
